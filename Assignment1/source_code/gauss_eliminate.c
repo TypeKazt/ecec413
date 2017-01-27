13f9b4ca51a27a147c1b9dd6e00b4c4102601155
hm375 <hm375@ECE-BSN615-L01.coe.drexel.edu>
2017-01-27 16:35:46 -0500

    added gaussian code from gold function

--- Assignment1/source_code/gauss_eliminate.c
+++ Assignment1/source_code/gauss_eliminate.c
@@ -85,6 +85,45 @@ main(int argc, char** argv) {
 void 
 gauss_eliminate_using_openmp(const Matrix A, Matrix U)                  /* Write code to perform gaussian elimination using OpenMP. */
 {
+	// TODO: add pragma for reduce current row
+	// TODO: add pragma for division step 
+
+
+	unsigned int i, j, k;
+		
+		for (i = 0; i < num_elements; i ++)             /* Copy the contents of the A matrix into the U matrix. */
+		    for(j = 0; j < num_elements; j++)
+		        U[num_elements * i + j] = A[num_elements*i + j];
+
+		
+		for (k = 0; k < num_elements; k++){             /* Perform Gaussian elimination in place on the U matrix. */
+		    for (j = (k + 1); j < num_elements; j++){   /* Reduce the current row. */
+
+				if (U[num_elements*k + k] == 0){
+					printf("Numerical instability detected. The principal diagonal element is zero. \n");
+					return 0;
+				}
+
+		        /* Division step. */
+				U[num_elements * k + j] = (float)(U[num_elements * k + j] / U[num_elements * k + k]);
+		    }
+		
+		    U[num_elements * k + k] = 1;             /* Set the principal diagonal entry in U to be 1. */
+		
+		    for (i = (k+1); i < num_elements; i++){
+		        for (j = (k+1); j < num_elements; j++)
+		            /* Elimnation step. */
+					U[num_elements * i + j] = U[num_elements * i + j] -\
+		                                      (U[num_elements * i + k] * U[num_elements * k + j]);
+			
+		        U[num_elements * i + k] = 0; 
+			} 
+		}
+
+		return 1;
+
+
+
 }
 
 
