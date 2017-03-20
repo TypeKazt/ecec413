3a1a13bb13b299ad9cc6460780c39095828c9e3a
Om Mahida <ommahida@gmail.com>
2017-03-19 21:27:37 -0400

    updated cuda

--- cuda_2/vector_dot_product.cu
+++ cuda_2/vector_dot_product.cu
@@ -43,9 +43,16 @@ void run_test(unsigned int num_elements)
 	}
 	
 	printf("Generating dot product on the CPU. \n");
-	
+
+	struct timeval start, stop;
+	gettimeofday(&start, NULL);
+
 	// Compute the reference solution on the CPU
 	float reference = compute_gold(A, B, num_elements);
+
+	gettimeofday(&stop, NULL);
+	printf("Execution time = %fs. \n", (float)(stop.tv_sec - start.tv_sec + (stop.tv_usec - start.tv_usec)/(float)1000000));
+
 
 	/* Edit this function to compute the result vector on the GPU. 
        The result should be placed in the gpu_result variable. */
@@ -71,8 +78,52 @@ void run_test(unsigned int num_elements)
 /* Edit this function to compute the dot product on the device. */
 float compute_on_device(float *A_on_host, float *B_on_host, int num_elements)
 {
-    float result = 0;
-    return result;
+	// Device vectors
+	float *A_on_device = NULL;
+	float *B_on_device = NULL;
+	float *C_on_device = NULL;
+
+	// allocate space and copy data to device for 3 vectors
+	cudaMalloc((void**)&A_on_device, num_elements*sizeof(float));
+	cudaMemcpy(A_on_device, A_on_host, num_elements*sizeof(float), cudaMemcpyHostToDevice);
+
+	cudaMalloc((void**)&B_on_device, num_elements*sizeof(float));
+	cudaMemcpy(B_on_device, B_on_host, num_elements*sizeof(float), cudaMemcpyHostToDevice);
+	
+	cudaMalloc((void**)&C_on_device, num_elements*sizeof(float));
+	cudaMemcpy(C_on_device, 0.0, GRID_SIZE*sizeof(float));
+	
+	// device mutex, add to each threadblock reduction
+	int *mutex = NULL;
+	cudaMalloc((void **)&mutex, sizeof(int));
+	cudaMemset(mutex, 0m sizeof(int));
+
+	// Thread block and grid inits
+	dim3 dimBlock(BLOCK_SIZE, 1, 1);
+	dim3 dimGrid(GRID_SIZE, 1);
+
+	printf("performing vector dot product on GPU. \n");
+	gettimeofday(&start, NULL);
+
+	vector_dot_product_kernel <<< dimGrid, dimBlock >>> (num_elements, A_on_device, B_on_device, C_on_device, mutex);
+	cudaThreadSynchronize();
+
+	gettimeofday(&stop, NULL);
+	printf("Execution time = %fs. \n", (float)(stop.tv_sec - start.tv_sec + (stop.tv_usec - start.tv_usec)/(float)1000000));
+
+	check_for_error("Error in kernel");
+	
+	/* Copy first element of C_on_device to result */
+	float result = 0.0;
+	cudaMemcpy( &result, C_on_device, sizeof(float), cudaMemcpyDeviceToHost);
+	
+	/* Free allocated vectors */
+	cudaFree(A_on_device);
+	cudaFree(B_on_device);
+	cudaFree(C_on_device);
+	
+	return result;
+
 }
  
 /* This function checks for errors returned by the CUDA run time. */
--- cuda_2/vector_dot_product_kernel.cu
+++ cuda_2/vector_dot_product_kernel.cu
@@ -1,11 +1,56 @@
 #ifndef _VECTOR_DOT_PRODUCT_KERNEL_H_
 #define _VECTOR_DOT_PRODUCT_KERNEL_H_
 
+#define BLOCK_SIZE 1024
+#define GRID_SIZE 1024
+
 /* Edit this function to complete the functionality of dot product on the GPU. 
 	You may add other kernel functions as you deem necessary. 
  */
+
+__device__ void lock(int *mutex);
+__device__ void unlock(int *mutex);
+
 __global__ void vector_dot_product_kernel()
 {
+	__shared__ float runningSums[BLOCK_SIZE];
+
+	int tx = threadIdx.x;
+	int threadID = blockDim.x * blockIdx.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
+
+	float local_thread_sum = 0.0;
+	unsigned int i = threadID;
+
+	while(i < num_elements){
+		local_thread_sum += a[i] * b[i];
+		i += stride;
+	}
+
+	runningSums[threadIdx.x] = local_thread_sum;
+	__syncthreads();
+
+	for(int stride = blockDim.x/2; stride > 0; stride /= 2){
+		if(tx < stride)
+			runningSums[tx] += runningSums[tx+stride];
+		__syncthreads();
+	}
+
+	if(threadIdx.x == 0) {
+		lock(mutex);
+		result[0] += runningSums[0];
+		unlock(mutex);
+	}
 }
+
+
+__device__ void lock(int *mutex){
+	while(atomicCAS(mutex, 0, 1) != 0);
+}
+
+__device__ void unlock(int *mutex){
+	atomicExch(mutex, 0);
+}
+
 
 #endif // #ifndef _VECTOR_DOT_PRODUCT_KERNEL_H
