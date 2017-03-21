066b4f8aed5d57cf2c9b1b41eea87f4d35f35c30
Om Mahida <ommahida@gmail.com>
2017-03-20 21:15:10 -0400

    trying to add sM global

--- cuda_3/2Dconvolution_kernel.cu
+++ cuda_3/2Dconvolution_kernel.cu
@@ -6,6 +6,9 @@
 #include "2Dconvolution.h"
 
 // __global__ void ConvolutionKernel(Matrix M, Matrix N, Matrix P, int matrix_size)
+__constant__ float sM[KERNEL_SIZE][KERNEL_SIZE];
+
+
 __global__ void ConvolutionKernel(Matrix M, Matrix N, Matrix P)
 
 {
@@ -57,7 +60,7 @@ __global__ void ConvolutionKernel(Matrix M, Matrix N, Matrix P)
 	// convolute
 	for (i = 0; i < KERNEL_SIZE; i ++)
 		for (j = 0; j < KERNEL_SIZE; j++)
-			sum += sN[ty + i][tx + j] * sN[i][j];
+			sum += sN[ty + i][tx + j] * sM[i][j];
 
 	if (tx < N.width && ty < N.height)
 		P.elements[tid] = sum;
