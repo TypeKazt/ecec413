9a5b7dd5e33aebab4b43ad6677f5efabd6bc57ff
Om Mahida <ommahida@gmail.com>
2017-03-26 00:28:51 -0400

    sc changes

--- final/sc/stream_compaction/compact_stream.cu
+++ final/sc/stream_compaction/compact_stream.cu
@@ -84,8 +84,8 @@ int compact_stream_on_device(float *result_d, float *h_data, unsigned int num_el
     float *result_device = NULL;
     float *h_device = NULL;
 
-    cudaMalloc((void**)&result_device, sizeof(int));
-    cudaMemcpy(n_device, n, sizeof(int));
+    cudaMalloc((void**)&n, sizeof(int));
+    cudaMemcpy(n_device, n, sizeof(int), cudaMemcpyHostToDevice);
 
     cudaMalloc((void**)&result_device, num_elements*sizeof(float));
     cudaMemcpy(result_device, result_d, num_elements*sizeof(float), cudaMemcpyHostToDevice);
@@ -113,6 +113,7 @@ int compact_stream_on_device(float *result_d, float *h_data, unsigned int num_el
     cudaMemcpy(n, n_device, sizeof(float), cudaMemcpyDeviceToHost);
     cudaMemcpy(h_data, h_device, num_elements*sizeof(float), cudaMemcpyDeviceToHost);
 
+    printf("size of h_device %f", sizeof(h_device));
     cudaFree(result_device);
     cudaFree(h_device);
     cudaFree(n_device);
